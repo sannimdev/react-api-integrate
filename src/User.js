e53358420135c81f97c1e390f15ebbaf9a454ee5
Sannim <inegg.apps@gmail.com>
2020-07-24 16:00:21 +0900

    커스텀 훅 대신 react-async를 이용하여 작업 처리하기

--- src/User.js
+++ src/User.js
@@ -1,17 +1,16 @@
 import React from "react";
 import axios from "axios";
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
 
-async function getUser(id) {
+async function getUser({ id }) {
   const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
   return response.data;
 }
 
 function User({ id }) {
-  const [state] = useAsync(() => getUser(id), [id]); //id값이 바뀔 때마다 함수를 호출하겠다
-  const { loading, data: user, error } = state;
+  const { data: user, error, isLoading } = useAsync({ promiseFn: getUser, id, watch: id }); //id값이 바뀔 때마다 함수를 호출하겠다
 
-  if (loading) return <div>로딩 중</div>;
+  if (isLoading) return <div>로딩 중</div>;
   if (error) return <div>오류가 발생했습니다. </div>;
   if (!user) return null;
 
--- src/Users.js
+++ src/Users.js
@@ -1,7 +1,15 @@
 import React, { useState, useReducer, useEffect } from "react";
 import axios from "axios";
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
 import User from "./User";
+
+/*
+react-async 바로 설치해서 사용할 수 있음. (컴포넌트 형태로도 사용 가능)
+특정 Promise를 기다리는 작업을 도중에 취소할 수도 있음.
+
+그러나 옵션이 복잡하다.
+커스텀 훅보다는 더 많은 기능을 제공하므로 옵션이 다양하다. 그래서 처음에 혼동할 수 있음.
+ */
 
 async function getUsers() {
   const response = await axios.get("https://jsonplaceholder.typicode.com/users/");
@@ -9,12 +17,13 @@ async function getUsers() {
 }
 
 function Users() {
-  const [state, refetch] = useAsync(getUsers, [], true);
   const [userId, setUserId] = useState(null);
-  const { loading, data: users, error } = state;
-  if (loading) return <div>로딩 중</div>;
+  const { data: users, error, isLoading, reload, run } = useAsync({
+    deferFn: getUsers,
+  });
+  if (isLoading) return <div>로딩 중</div>;
   if (error) return <div>오류 발생...</div>;
-  if (!users) return <button onClick={refetch}>불러오기</button>;
+  if (!users) return <button onClick={run}>불러오기</button>;
 
   return (
     <>
@@ -25,7 +34,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={refetch}>다시 불러오기</button>
+      <button onClick={reload}>다시 불러오기</button>
       {userId && <User id={userId} />}
     </>
   );
